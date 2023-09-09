e0f57c500e7678d2733aae756c46b85266bc8326
arielyance <arielyanceprueba@gmail.com>
2023-09-08 21:04:24 -0500

    version final del codigo

--- Version1_jetbot.py
+++ Version1_jetbot.py
@@ -15,45 +15,39 @@ def bajar():
     
 def subir():
     TTLServo.servoAngleCtrl(3, 10, 1, 150)
-    tm.sleep(.5)
+    tm.sleep(.1)
     TTLServo.servoAngleCtrl(2, 20, 1, 150)
     
 def agarrar():
     TTLServo.servoAngleCtrl(4, -34, 1, 150)#cerrar griper
-    tm.sleep(.5)
+    tm.sleep(.1)
 
 def soltar():
     TTLServo.servoAngleCtrl(4, 0, 1, 150)#cerrar griper
-    tm.sleep(.5)
+    tm.sleep(.1)
 
 # funciones motor
-def avanzar(change):
-    if change['new'] == 1:
-        robot.left(0.5)
-        robot.right(0.5)
-    else:
-        robot.stop()
+def avanzar():
+    robot.forward(.6)
 
-def izquierda(change):
-    if change['new'] == 1:
-        robot.left(0.3)
-        robot.right(-0.3)
-    else:
-        robot.stop()
+def reversa():
+    robot.backward(.6)    
 
-def derecha(change):
-    if change['new'] == 1:
-        robot.left(-0.3)
-        robot.right(0.3)
-    else:
-        robot.stop()
-
-
+def izquierda():
+    robot.left(0.3)
+    robot.right(-0.3)
+    
+def derecha():
+    robot.left(-0.3)
+    robot.right(0.3)
+def parar():
+    robot.stop()
 #loop
 robot = Robot()
 
 #ruedas
 controller.buttons[6].observe(lambda change: avanzar(), names='value')
+controller.buttons[7].observe(lambda change: reversa(), names='value')
 controller.buttons[4].observe(lambda change: izquierda(), names='value')
 controller.buttons[5].observe(lambda change: derecha(), names='value')
 
